1ad31d7635106193b45d93f37906d47df8ec404c
DESKTOP-304U2MR\r2com <thvldk1654@naver.com>
2024-11-19 05:56:13 +0900

    feat : order_item_id 더미 로직 추가

--- src/main/java/com/shoppingoo/brand/domain/review/dto/ReviewResponse.java
+++ src/main/java/com/shoppingoo/brand/domain/review/dto/ReviewResponse.java
@@ -1,26 +1,27 @@
 package com.shoppingoo.brand.domain.review.dto;
 
 import java.time.LocalDateTime;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+
+import lombok.*;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
+@Getter
+@Setter
 public class ReviewResponse {
-    private Integer id;                 // Review의 기본 키 필드
-    private Integer orderItemId;
-    private Integer productCode;
-    private String userNickname;
-    private Integer height;
-    private Integer weight;
+    private int reviewId;                 // Review의 기본 키 필드
+    private int orderItemId;
+    private int productCode;
+    private int userId;
+    private String username;
+    private int height;
+    private int weight;
     private String option;
     private String content;
     private String size;
-    private Integer starRating;         // null 가능성 있으므로 Integer 사용
+    private int starRating;         // null 가능성 있으므로 int 사용
     private String color;
     private String imageUrl;
     private LocalDateTime reviewDate;   // 리뷰 작성 날짜
