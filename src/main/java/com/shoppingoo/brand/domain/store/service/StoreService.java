b0eaf773663acc295e1fb90351f67e2a659cd2b1
Sunny Lee <86199470+sunnnycode@users.noreply.github.com>
2024-11-19 11:02:40 +0900

    feat: 가게 상세 조회 API 개발

--- src/main/java/com/shoppingoo/brand/domain/store/service/StoreService.java
+++ src/main/java/com/shoppingoo/brand/domain/store/service/StoreService.java
@@ -1,5 +1,6 @@
 package com.shoppingoo.brand.domain.store.service;
 
+import com.shoppingoo.brand.domain.product.dto.ProductResponse;
 import com.shoppingoo.brand.domain.store.dto.StatusRequest;
 import com.shoppingoo.brand.domain.store.dto.StoreRequest;
 import com.shoppingoo.brand.domain.store.dto.StoreResponse;
@@ -11,5 +12,5 @@ public interface StoreService {
     StoreResponse storeRegister(int userId, StoreRequest storeRequest);
     List<StoreResponse> getAllStores();
     StoreResponse updateStoreStatus(int storeId, int userId, StatusRequest statusRequest);
-
+    StoreResponse getStoreById(int storeId);
 }
