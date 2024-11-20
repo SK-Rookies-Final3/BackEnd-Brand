2a1aca6365eeea66c231a48c9b4abb89403b7eb8
DESKTOP-304U2MR\r2com <thvldk1654@naver.com>
2024-11-20 10:43:11 +0900

    fix : open-api, api 구분 및 image_url body로 구분

--- src/main/java/com/shoppingoo/brand/domain/review/service/ReviewService.java
+++ src/main/java/com/shoppingoo/brand/domain/review/service/ReviewService.java
@@ -6,7 +6,7 @@ import com.shoppingoo.brand.domain.review.dto.ReviewResponse;
 import java.util.List;
 
 public interface ReviewService {
-    ReviewResponse reviewRegister(int productCode, int userId, String imageUrl, ReviewRequest reviewRequest);
+    ReviewResponse reviewRegister(int productCode, int userId, ReviewRequest reviewRequest);
     List<ReviewResponse> getReviewsByProductCode(int productCode);
     void reviewDelete(int reviewId, int userId); // 사용자 ID를 포함하는 수정된 메서드 시그니처
 }
