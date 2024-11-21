b4f63e76539c2ccfec6ed4c1bc9666e5c430cc7b
DESKTOP-304U2MR\r2com <thvldk1654@naver.com>
2024-11-21 17:17:03 +0900

    fix : eureka off

--- src/main/java/com/shoppingoo/brand/BrandApplication.java
+++ src/main/java/com/shoppingoo/brand/BrandApplication.java
@@ -3,11 +3,8 @@ package com.shoppingoo.brand;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 //import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-//import org.springframework.boot.autoconfigure.domain.EntityScan;
-
-
-@SpringBootApplication
 //@EnableDiscoveryClient
+@SpringBootApplication
 public class BrandApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(BrandApplication.class, args);
