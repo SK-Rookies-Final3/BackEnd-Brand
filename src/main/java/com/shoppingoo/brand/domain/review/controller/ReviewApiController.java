bac1b1901f6269088724dfdbd35b7a8e6d106919
DESKTOP-304U2MR\r2com <thvldk1654@naver.com>
2024-11-21 15:27:13 +0900

    fix : api/brand 추가

--- src/main/java/com/shoppingoo/brand/domain/review/controller/ReviewApiController.java
+++ src/main/java/com/shoppingoo/brand/domain/review/controller/ReviewApiController.java
@@ -10,8 +10,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
 @RestController
-@RequestMapping("/api/review")
-@CrossOrigin(origins = "http://localhost:3000")
+@RequestMapping("/api/brand/review")
 public class ReviewApiController {
 
     private final ReviewService reviewService;
