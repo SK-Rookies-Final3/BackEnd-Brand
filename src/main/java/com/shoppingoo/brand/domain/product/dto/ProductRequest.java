77f0cbc83d804991b0eed3b0294a58e4779fd715
Sunny Lee <86199470+sunnnycode@users.noreply.github.com>
2024-11-23 11:28:55 +0900

    fix: product 에서 category의 enum 삭제

--- src/main/java/com/shoppingoo/brand/domain/product/dto/ProductRequest.java
+++ src/main/java/com/shoppingoo/brand/domain/product/dto/ProductRequest.java
@@ -1,6 +1,5 @@
 package com.shoppingoo.brand.domain.product.dto;
 
-import com.shoppingoo.brand.db.product.enums.Category;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -24,7 +23,7 @@ public class ProductRequest {
 
     private String imageInformation;
 
-    private Category category;
+    private String category;
 
     private String color;
 
