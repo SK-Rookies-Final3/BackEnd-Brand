9b313f0d36382019dc350f6a79bf292047c5a608
Hwang Seo Jeong <129826956+hsj1024@users.noreply.github.com>
2024-11-28 15:41:44 +0900

    Update schema.sql

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,4 +1,3 @@
 CREATE DATABASE IF NOT EXISTS branddb;
 USE branddb;
-INSERT INTO store (id, user_id, name, license_number, status, registered_at)
-VALUES (50001, 1, 'Example Store', 12345, 0, NOW());
+ALTER TABLE store AUTO_INCREMENT = 50001;
